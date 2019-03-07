ddaad8d43f77c2360b37dc1f3fb0ca985c73c484
Bas Beelen <basbeelen@godatadriven.com>
2019-03-07 11:46:04 +0100

    Reformat add ssh connection to Airflow

--- envs/sftp-mysql-example/preparations/01_cp_mock_data_to_sftp.sh
+++ envs/sftp-mysql-example/preparations/01_cp_mock_data_to_sftp.sh
@@ -47,7 +47,13 @@ echo "===================================================="
 echo "== Put mock-data on ftp server ====================="
 echo "===================================================="
 
-airflow connections -a --conn_id ftp_server --conn_type SSH --conn_host ftp-server --conn_login $SFTP_USER --conn_port 22 --conn_password $SFTP_PASS
+airflow connections -a --conn_id ftp_server \
+                       --conn_type SSH \
+                       --conn_host ftp-server \
+                       --conn_login $SFTP_USER \
+                       --conn_port 22 \
+                       --conn_password $SFTP_PASS
+
 mkdir -p ~/.ssh && ssh-keyscan ftp-server >> ~/.ssh/known_hosts
 
 apt-get install -y sshpass
