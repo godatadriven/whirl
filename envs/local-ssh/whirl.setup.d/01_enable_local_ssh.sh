121911ec76c20815b5486760f1855069204fec9b
Kris Geusebroek <krisgeus@gmail.com>
2019-06-05 09:16:39 +0200

    Make sure example local ssh works with python based airflow container

--- envs/local-ssh/whirl.setup.d/01_enable_local_ssh.sh
+++ envs/local-ssh/whirl.setup.d/01_enable_local_ssh.sh
@@ -4,15 +4,17 @@ echo "======================================"
 echo "== Configure SSH ====================="
 echo "======================================"
 
-airflow connections -a --conn_id local_ssh --conn_type SSH --conn_host localhost --conn_login root --conn_port 22
+airflow connections -a --conn_id local_ssh --conn_type SSH --conn_host localhost --conn_login airflow --conn_port 22
 
 # Enable SSH to localhost and install postgresql-client
-rm -f /root/.ssh/id_rsa*
-ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -N ""
-cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-chmod og-wx /root/.ssh/authorized_keys
-apt-get install -y openssh-client openssh-server
-echo "ALL: localhost" >> /etc/hosts.allow
+mkdir -p ${HOME}/.ssh
+rm -f ${HOME}/.ssh/id_rsa*
+sudo apt-get update
+sudo apt-get install -y openssh-client openssh-server
+ssh-keygen -t rsa -f ${HOME}/.ssh/id_rsa -q -N ""
+cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
+chmod og-wx ${HOME}/.ssh/authorized_keys
+sudo sh -c 'echo "ALL: localhost" >> /etc/hosts.allow'
 
-service ssh restart
-ssh-keyscan -H localhost >> /root/.ssh/known_hosts
+sudo service ssh restart
+ssh-keyscan -H localhost >> ${HOME}/.ssh/known_hosts
