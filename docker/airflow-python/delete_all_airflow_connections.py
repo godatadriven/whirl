d18adc8f0759443d4e7576e6cb81b3cae4ce9b01
Kris Geusebroek <krisgeus@gmail.com>
2019-06-06 11:09:21 +0200

    Add suggested change by Fokko

--- docker/airflow-python/delete_all_airflow_connections.py
+++ docker/airflow-python/delete_all_airflow_connections.py
@@ -3,11 +3,8 @@
 Removing all current airflow connections
 """
 
-from airflow import settings
 from airflow.models import Connection
+from airflow.utils.db import create_session
 
-session = settings.Session()
-session.query(Connection).delete()
-session.commit()
-session.close()
-exit()
+with create_session() as session:
+    session.query(Connection).delete()
