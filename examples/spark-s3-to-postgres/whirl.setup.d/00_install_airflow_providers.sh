0654d1b3b4f3e2ce7e8439ae5c60d894bdcdc5f7
Daniel van der Ende <daniel.vanderende@gmail.com>
2021-01-25 16:23:54 +0100

    Update docs, remove duplicate setup step

--- examples/spark-s3-to-postgres/whirl.setup.d/00_install_airflow_providers.sh
+++ examples/spark-s3-to-postgres/whirl.setup.d/00_install_airflow_providers.sh
@@ -5,4 +5,3 @@ echo "== Install required airflow providers =="
 echo "========================================"
 
 pip install apache-airflow-providers-apache-spark
-aws s3api put-object --bucket ${DEMO_BUCKET} --key input/data/demo/spark/$(date "+%Y%m%d")/data.json --body /mock-data/input.json
