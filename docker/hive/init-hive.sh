e315a7c3ca4eabafdd0395c5635fcfb6e06d783c
Jasper Ginn <jasperginn@gmail.com>
2021-02-26 18:10:58 +0100

    replacing env variables with actual values

--- docker/hive/init-hive.sh
+++ docker/hive/init-hive.sh
@@ -2,6 +2,15 @@
 
 set +ex
 
+sleep 30
+
+sed -e "s/AWS_SERVER/$AWS_SERVER/g" \
+    -e "s/AWS_PORT/$AWS_PORT/g" \
+    -e "s/AWS_ACCESS_KEY_ID/$AWS_ACCESS_KEY_ID/g" \
+    -e "s/AWS_SECRET_ACCESS_KEY/$AWS_SECRET_ACCESS_KEY/g" \
+    -e "s/HIVE_DW_BUCKET/$HIVE_DW_BUCKET/g" \
+    -i $HIVE_HOME/conf/core-site.xml
+
 $HIVE_HOME/bin/schematool -dbType derby -initSchema
 $HIVE_HOME/hcatalog/sbin/hcat_server.sh start &
 
