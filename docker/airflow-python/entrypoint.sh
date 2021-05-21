bfb929af5815d292760831d299a76b5457214ed4
Kris Geusebroek <krisgeus@gmail.com>
2021-05-21 20:28:04 +0200

    Improve CI mode. Airflow REST api is used to retrieve the dagrun status. Dagrun success yields exitstatus 0.

--- docker/airflow-python/entrypoint.sh
+++ docker/airflow-python/entrypoint.sh
@@ -39,23 +39,6 @@ for filename in ${WHIRL_SETUP_FOLDER}/dag.d/*.sh; do
   . "$filename"
 done
 
-if [[ ${AIRFLOW_COMMAND} == "webserver" || ${AIRFLOW_COMMAND} == "singlemachine" ]]; then
-
-  if [ "${UNPAUSE_DAG}" = true ]; then
-    echo "================================="
-    echo "== Enabling all available DAGs =="
-    echo "================================="
-    # Airflow "helpfully" prints a bunch of logging before the DAG names when you run the list-dags command. The bit of info
-    # we're looking for is preceded by "-------\nDAGS\n--------\n". The awk script suppresses all output until it has seen
-    # two dashed lines, after which it prints every line.
-    for d in $(airflow list_dags 2>/dev/null | awk 'START{ line = 0 }/----/{ line += 1 }!/----/{ if (line >= 2) print $0 }')
-    do
-      echo "Enabling DAG ${d}"
-      airflow unpause "${d}" || true
-    done
-  fi
-fi
-
 if [[ ${AIRFLOW_COMMAND} == "singlemachine" ]]; then
   nohup /entrypoint scheduler -D &
   # echo  "wait a while for the scheduler to be started"
