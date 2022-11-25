6912aa2c236cb902eef0eabb69178a75757c4880
Tim Steinkühler <timsteinkuhler@godatadriven.com>
2022-11-25 14:53:06 +0100

    Ingest Airflow metadata into datahub

--- examples/datahub-metadata/README.md
+++ examples/datahub-metadata/README.md
@@ -1,3 +1,21 @@
 #
 [Datahub Quickstart Guide](https://datahubproject.io/docs/quickstart)
 
+## Adding metadata
+
+### Postgres
+
+Set `export POSTGRES_PASSWORD=p@ssw0rd`
+[Edit this file](./metadata_recipes/postgres_recipe.yaml)
+`datahub ingest -c metadata_recipes/postgres_recipe.yaml`
+
+### Airflow
+
+- Follow steps [here](https://datahubproject.io/docs/lineage/airflow), to set up airflow with DataHub, this will allow Airflow to sync metadata with DataHub.
+- Add a description to your DAG object
+
+## Looking at metadata
+- Airflow
+  - See pipeline description and lineage
+  - tasks -> see run
+  - "View in UI"
--- examples/datahub-metadata/dag.py
+++ examples/datahub-metadata/dag.py
@@ -1,3 +1,9 @@
+"""
+# Mock API - S3 - postgres
+
+This DAG pulls data from a mock API, stores it on S3 and then pushes it to Postgres
+"""
+
 from datetime import timedelta, datetime
 from airflow import DAG
 
@@ -6,12 +12,14 @@ from airflow.hooks.http_hook import HttpHook
 from airflow.hooks.S3_hook import S3Hook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+from datahub_provider.entities import Dataset
+
 from sqlalchemy import create_engine
+from textwrap import dedent
 
 import pandas as pd
 
 import os
-import s3fs
 
 default_args = {
     'owner': 'whirl',
@@ -60,7 +68,7 @@ def _demo_s3_to_postgress(conn_id, templates_dict, **context):
     s3_path = f"s3://{templates_dict['s3_bucket']}/{templates_dict['s3_input_path']}"
     print(s3_path)
     df = pd.read_parquet(
-        f"s3://{templates_dict['s3_bucket']}/{templates_dict['s3_input_path']}",
+        s3_path,
         storage_options={
             "client_kwargs":{"endpoint_url": "http://s3server:4563"}
         },
@@ -74,61 +82,97 @@ def _demo_s3_to_postgress(conn_id, templates_dict, **context):
     )
     conn = pg_hook.get_connection(templates_dict['pg_conn_id'])
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
-    df.to_sql(templates_dict['target_table'], engine)
+    df.to_sql(templates_dict['target_table'], engine, if_exists="replace")
 
 
 local_path = "/tmp/whirl-local-api-to-s3-example/demo-api.parquet"
 s3bucket = "demo-s3-output"
 output_path = "api-store/{{ ds_nodash }}/demo-api.parquet"
 
-dag = DAG(dag_id='whirl-local-api-to-s3-example',
-          default_args=default_args,
-          schedule_interval='@once',
-          dagrun_timeout=timedelta(seconds=120))
+with DAG(
+    dag_id='whirl-local-api-s3-postgres',
+    default_args=default_args,
+    schedule_interval='@once',
+    dagrun_timeout=timedelta(seconds=120)
+) as dag:
+    dag.doc_md = __doc__  # takes docstring from the beginning of the DAG file
 
-api_get = PythonOperator(
-    task_id="api_get",
-    python_callable=_demo_api_get,
-    op_kwargs={
-        "conn_id": "local_api",
-    },
-    templates_dict={"localfile": local_path},
-    provide_context=True,
-    dag=dag,
-)
+    api_get = PythonOperator(
+        task_id="api_get",
+        python_callable=_demo_api_get,
+        op_kwargs={
+            "conn_id": "local_api",
+        },
+        templates_dict={"localfile": local_path},
+        provide_context=True,
+    )
 
-store_s3 = PythonOperator(
-    task_id="store_s3",
-    python_callable=_demo_s3_store,
-    op_kwargs={
-        "conn_id": "local_s3",
-    },
-    templates_dict={
-        "localfile": local_path,
-        "s3_bucket": s3bucket,
-        "s3_output_path": output_path
-    },
-    provide_context=True,
-    dag=dag
-)
+    api_get.doc_md = dedent(
+        """\
+    ## api_get Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+    **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
+    """
+    )
 
-# todo, specify inlet and outlet datasets
 
-s3_to_postgres = PythonOperator(
-    task_id="s3_to_postgres",
-    python_callable=_demo_s3_to_postgress,
-    op_kwargs={
-        "conn_id": "local_s3",
-    },
-    templates_dict={
-        "s3_bucket": s3bucket,
-        "s3_input_path": output_path,
-        "pg_conn_id": "local_pg",
-        "target_database": os.environ["POSTGRES_DB"],
-        "target_table": "api",
-    },
-    provide_context=True,
-    dag=dag
-)
 
-api_get >> store_s3 >> s3_to_postgres
+    store_s3 = PythonOperator(
+        task_id="store_s3",
+        python_callable=_demo_s3_store,
+        op_kwargs={
+            "conn_id": "local_s3",
+        },
+        templates_dict={
+            "localfile": local_path,
+            "s3_bucket": s3bucket,
+            "s3_output_path": output_path
+        },
+        provide_context=True,
+    )
+
+    store_s3.doc_md = dedent(
+        """\
+    ## store_s3 Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+    **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
+    """
+    )
+
+    # todo, specify inlet and outlet datasets
+
+    s3_to_postgres = PythonOperator(
+        task_id="s3_to_postgres",
+        python_callable=_demo_s3_to_postgress,
+        op_kwargs={
+            "conn_id": "local_s3",
+        },
+        templates_dict={
+            "s3_bucket": s3bucket,
+            "s3_input_path": output_path,
+            "pg_conn_id": "local_pg",
+            "target_database": os.environ["POSTGRES_DB"],
+            "target_table": "api",
+        },
+        provide_context=True,
+        outlets=[Dataset("postgres", "postgresdb.public.api")],
+    )
+
+    s3_to_postgres.doc_md = dedent(
+        """\
+    ## s3_to_postgres Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+    **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
+    """
+    )
+
+    api_get >> store_s3 >> s3_to_postgres
