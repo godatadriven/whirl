bc2c135e71111921369b44c47befd95d7d74b5df
Kris Geusebroek <krisgeus@gmail.com>
2023-07-21 16:44:49 +0200

    Set correct permissions

--- examples/dbt-example/whirl.setup.d/02_install_postgres_dbt.sh
+++ examples/dbt-example/whirl.setup.d/02_install_postgres_dbt.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 pip install dbt-postgres
 
-sudo chown -R airflow:root /opt/airflow/dags/dbt-example/dbt
-sudo chmod -R 644 /opt/airflow/dags/dbt-example/dbt
+sudo chmod -R ugo+rw /opt/airflow/dags/dbt-example/dbt
--- examples/dbt-spark-example/whirl.setup.d/02_add_dbt.sh
+++ examples/dbt-spark-example/whirl.setup.d/02_add_dbt.sh
@@ -2,5 +2,4 @@
 sudo apt-get install -y libsasl2-dev build-essential
 pip install agate==1.6.1 airflow-dbt dbt-spark[PyHive]
 
-sudo chown -R airflow:root /opt/airflow/dags/dbt-spark-example/dbt
-sudo chmod -R 644 /opt/airflow/dags/dbt-spark-example/dbt
+sudo chmod -R ugo+rw /opt/airflow/dags/dbt-spark-example/dbt
