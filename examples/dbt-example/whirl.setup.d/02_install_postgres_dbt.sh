53c0af3a8bb90dc0d8528e4363f8aa5c0e67ec4c
Kris Geusebroek <krisgeus@gmail.com>
2023-07-21 11:26:50 +0200

    Fix DBT examples

--- examples/dbt-example/whirl.setup.d/02_install_postgres_dbt.sh
+++ examples/dbt-example/whirl.setup.d/02_install_postgres_dbt.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pip install dbt-postgres
 
-sudo chown airflow:root /opt/airflow/dags/dbt-example/dbt/logs
+sudo chown -R airflow:root /opt/airflow/dags/dbt-example/dbt
+sudo chmod -R 755 /opt/airflow/dags/dbt-example/dbt
--- examples/dbt-spark-example/whirl.setup.d/02_add_dbt.sh
+++ examples/dbt-spark-example/whirl.setup.d/02_add_dbt.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install -y libsasl2-dev build-essential
 pip install agate==1.6.1 airflow-dbt dbt-spark[PyHive]
+
+sudo chown -R airflow:root /opt/airflow/dags/dbt-spark-example/dbt
+sudo chmod -R 755 /opt/airflow/dags/dbt-spark-example/dbt
