220b9657171d790b5aa7b15a939dda5750b07021
Kris Geusebroek <krisgeus@gmail.com>
2023-08-25 13:46:36 +0200

    Prevent failure in show log

--- examples/dbt-spark-example/compose.teardown.d/01_show_dbt_log.sh
+++ examples/dbt-spark-example/compose.teardown.d/01_show_dbt_log.sh
@@ -7,9 +7,9 @@ function show_logs() {
     local SCRIPT_DIR=$( dirname ${BASH_SOURCE[0]} )
     DBT_LOG_DIR="${SCRIPT_DIR}/../dbt/logs"
 
-    if [ "$(ls -A ${DBT_LOG_DIR})" ]; then
-        echo "${DBT_LOG_DIR} is not empty. Showing log!!"
-        sudo cat ${DBT_LOG_DIR}/dbt.log || true
+    if [ "$(ls -A ${DBT_LOG_DIR}/dbt.log)" ]; then
+        echo "${DBT_LOG_DIR}/dbt.log exists. Showing log!!"
+        sudo cat ${DBT_LOG_DIR}/dbt.log
     fi
 }
 
