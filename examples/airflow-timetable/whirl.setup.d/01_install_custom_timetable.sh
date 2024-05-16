a0cae69f06d3e96762cf3b091f276816cb53fee6
Kris Geusebroek <krisgeus@gmail.com>
2024-05-16 15:22:43 +0200

    Bump ephem version to be compatible with py3.12

--- examples/airflow-timetable/whirl.setup.d/01_install_custom_timetable.sh
+++ examples/airflow-timetable/whirl.setup.d/01_install_custom_timetable.sh
@@ -5,5 +5,4 @@ echo "== Install timetable plugins =="
 echo "==============================="
 
 mkdir -p /tmp/custom_build
-sudo apt-get update && sudo apt-get install -y build-essential
 pip install ${WHIRL_SETUP_FOLDER}/dag.d/plugins
