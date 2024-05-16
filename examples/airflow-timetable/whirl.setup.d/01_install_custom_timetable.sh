60445acb1c213f82981e59bafebdfcbd49ba510c
Kris Geusebroek <krisgeus@gmail.com>
2024-05-16 14:36:06 +0200

    Add gcc to build custom timetable package

--- examples/airflow-timetable/whirl.setup.d/01_install_custom_timetable.sh
+++ examples/airflow-timetable/whirl.setup.d/01_install_custom_timetable.sh
@@ -5,4 +5,5 @@ echo "== Install timetable plugins =="
 echo "==============================="
 
 mkdir -p /tmp/custom_build
+sudo apt-get update && sudo apt-get install -y build-essential
 pip install ${WHIRL_SETUP_FOLDER}/dag.d/plugins
