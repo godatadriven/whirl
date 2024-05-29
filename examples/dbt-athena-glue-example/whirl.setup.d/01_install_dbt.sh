792c9f68c9cbb22d3f5a06c12e506bafd511f845
Kris Geusebroek <krisgeus@gmail.com>
2024-05-29 15:32:01 +0200

    Make sure dependencies are installed

--- examples/dbt-athena-glue-example/whirl.setup.d/01_install_dbt.sh
+++ examples/dbt-athena-glue-example/whirl.setup.d/01_install_dbt.sh
@@ -5,4 +5,5 @@ pip install astronomer-cosmos[dbt-athena]==1.4.1
 sudo chmod -R ugo+rw /opt/airflow/dags/dbt-athena-glue-example/dbt
 
 cd /opt/airflow/dags/dbt-athena-glue-example/dbt
+dbt deps
 dbt parse
