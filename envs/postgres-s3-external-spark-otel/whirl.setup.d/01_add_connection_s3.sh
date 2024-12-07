ee4c21ef28cf399df540445d91ce51dfded23066
Barend Garvelink <159024183+barend-xebia@users.noreply.github.com>
2024-12-07 19:38:54 +0100

    simplify wait for S3

--- envs/postgres-s3-external-spark-otel/whirl.setup.d/01_add_connection_s3.sh
+++ envs/postgres-s3-external-spark-otel/whirl.setup.d/01_add_connection_s3.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 echo "=================="
 echo "== Configure S3 =="
@@ -8,16 +9,22 @@ pip install --disable-pip-version-check --progress-bar=raw --no-cache-dir awscli
 
 echo -e "$AWS_ACCESS_KEY_ID\n$AWS_SECRET_ACCESS_KEY\n\n" | aws configure
 aws configure set plugins.endpoint awscli_plugin_endpoint
-aws configure set default.s3.endpoint_url http://${AWS_SERVER}:${AWS_PORT}
-aws configure set default.s3api.endpoint_url http://${AWS_SERVER}:${AWS_PORT}
+aws configure set default.s3.endpoint_url "http://${AWS_SERVER}:${AWS_PORT}"
+aws configure set default.s3api.endpoint_url "http://${AWS_SERVER}:${AWS_PORT}"
 
 echo "======================"
 echo "== Create S3 Bucket =="
 echo "======================"
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${AWS_SERVER}:${AWS_PORT})" != "200" ]]; do
-  echo "Waiting for ${AWS_SERVER} to come up..."
-  sleep 2;
-done
 
-echo "creating bucket"
+echo "Waiting for ${AWS_SERVER} to come up..."
+curl --fail \
+     --silent \
+     --retry 30 \
+     --retry-delay 5 \
+     --output /dev/null \
+     --connect-timeout 5 \
+     --retry-connrefused \
+     --url "http://${AWS_SERVER}:${AWS_PORT}"
+
+echo "Creating bucket"
 aws s3api create-bucket --bucket ${DEMO_BUCKET}
